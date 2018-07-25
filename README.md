da21bb9c36cceab7a7ffc698a4d76d425c15a4bd
Zdenek Obst <zdenek.obst@gmail.com>
2018-07-25 10:29:16 +0200

    Migration link fix

---
+++ README.md
@@ -0,0 +1,3 @@
+Migrated to GitLab
+==================
+I no longer use GitHub, all my projects can be found on GitLab here: **https://gitlab.com/zdenda.online**
--- m4a_to_mp3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This script iterates all *.m4a files in working directory and converts them to MP3 audio format.
-
-for a in *.m4a; do
-  faad -o - "$a" | lame - "${a%.m4a}.mp3"
-done
-
